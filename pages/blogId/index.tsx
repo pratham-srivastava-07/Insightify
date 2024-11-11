a2d2a55c87d60546aa7e212360c0f8e578785c06
Krish <prathamrajsri2003@gmail.com>
2024-11-11 07:01:41 +0530

    api fix

--- pages/blogId/index.tsx
+++ pages/blogId/index.tsx
@@ -1,12 +1,15 @@
 import axios from "axios"
 
 async function getBlogDetails() {
-    const response = await axios.get('http://localhost:3000/api/blogs/:blogId')
+    const response = await axios.get('http://localhost:3000/api/blog/:blogId')
     return response.data
 }
 
 export default async function BlogId() {
     const data = await getBlogDetails()
+    if(!data) {
+        return ""
+    }
     return <div>
         {data?.title}
         {data?.imageUrl}
